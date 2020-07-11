1b9a01733f8787ba1d7374e207a39080c87282c4
Tigran Asriyan <tigran.asriyan@gmail.com>
2020-07-11 00:01:11 -0400

    add log

--- server.js
+++ server.js
@@ -1,17 +1,18 @@
-require ('dotenv').config();
+require("dotenv").config();
 
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const path = require('path');
-const buildPath = ('build/index.html');
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const path = require("path");
+const buildPath = "build/index.html";
 const port = process.env.PORT || 5000;
 
-const sendGrid = require('@sendgrid/mail');
+const sendGrid = require("@sendgrid/mail");
 const server = express();
 server.use(bodyParser.json());
 
 server.use(cors());
+server.use(express.json());
 
 // server.use((req, res, next) => {
 //   res.setHeader('Access-Control-Allow-Origin', '*');
@@ -23,7 +24,7 @@ server.use(cors());
 // server.get('/api', (req, res, next) => {
 //   res.send('API Status: Running')
 // });
-
+console.log(__dirname);
 
 server.use(express.static(`${__dirname}/client/public`));
 // server.use('/static', express.static('public'))
@@ -32,36 +33,37 @@ server.use(express.static(`${__dirname}/client/public`));
 // server.get('*', (req, res) => {
 //     res.sendFile(path.resolve(__dirname, 'index.html'));
 // });
-console.log(express.static('client/build'));
-console.log(`${__dirname}/client/public/index.html`);
-server.use(express.json());
 
-const REACT_APP_SENDGRID_API_KEY =`${process.env.REACT_APP_SENDGRID_API_KEY}`
-server.post('/api/email', (req, res, next) => {
+// console.log(express.static('client/build'));
+// console.log(`${__dirname}/client/public/index.html`);
+
+const REACT_APP_SENDGRID_API_KEY = `${process.env.REACT_APP_SENDGRID_API_KEY}`;
+server.post("/api/email", (req, res, next) => {
   sendGrid.setApiKey(REACT_APP_SENDGRID_API_KEY);
   console.log(req.body);
   const msg = {
-      to: 'kevgill95@gmail.com',
-      from: req.body.email,
-      subject: req.body.subject,
-      text: req.body.message
-  }
+    to: "kevgill95@gmail.com",
+    from: req.body.email,
+    subject: req.body.subject,
+    text: req.body.message,
+  };
 
-  sendGrid.send(msg)
-      .then(result => {
-          res.status(200).json({
-            success: true
-          });
-
-      })
-      .catch(err => {
-          console.log('error: ', err);
-          res.status(401).json({
-            success: false
-          })
-      })
+  sendGrid
+    .send(msg)
+    .then((result) => {
+      res.status(200).json({
+        success: true,
+      });
+    })
+    .catch((err) => {
+      console.log("error: ", err);
+      res.status(401).json({
+        success: false,
+      });
+    });
 });
 
 server.listen(port, () => {
   console.log(`Server is up on port ${port}!`);
+  console.log(__dirname);
 });
