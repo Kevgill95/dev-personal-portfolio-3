dd9569cc31cbbbe40fcd70497530dc4f2e63fb98
Kevin <kgilloo1@friars.providence.edu>
2020-07-13 16:18:34 -0400

    fading in transition

--- front/src/components/Hero.js
+++ front/src/components/Hero.js
@@ -7,11 +7,11 @@ import "../css/app.css";
 function Hero(props) {
 
         return(
-            <Jumbotron  className="d-flex text-center bg-transparent jumbotron-fluid p-0">
-                <Container fluid={true} className="justify-content-center py-5">
-                    { props.title && <h1 className="display-1 font-weight-bolder">{props.title}</h1> }
-                    { props.subTitle && <h3 className="display-4 font-weight-light">{props.subTitle}</h3> }
-                    { props.label && <h3 className="lead font-weight-light">{props.label}</h3> }
+            <Jumbotron className="d-flex text-center bg-transparent jumbotron-fluid p-0">
+                <Container fluid={true} className="justify-content-center py-5 textFadeInDown">
+                    { props.title && <h1 className="display-1 font-weight-bolder textFadeInDown">{props.title}</h1> }
+                    { props.subTitle && <h3 className="display-4 font-weight-light textFadeInDown">{props.subTitle}</h3> }
+                    { props.label && <h3 className="lead font-weight-light textFadeInDown">{props.label}</h3> }
                 </Container>
             </Jumbotron>
     );
